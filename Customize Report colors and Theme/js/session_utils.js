b6d1c267aa2850f39916e56047a3f530fcd6e6a5
Parth Mangukiya <v-parman@microsoft.com>
2021-01-25 05:30:18 +0000

    Merged PR 138178: Update the code for refreshing the access token for the embedded report
    This PR updates the code to refresh the access token for the embedded report when it gets expired.
    
    This functionality has been completed in all the five showcases
    
    Related work items: #508360, #509920, #510398, #512921, #514919, #515647

--- Customize Report colors and Theme/js/session_utils.js
+++ Customize Report colors and Theme/js/session_utils.js
@@ -1,11 +1,14 @@
-// API Endpoint to get the JSON response of Embed Url, Embed token and reportId
-const reportUrl = "https://aka.ms/ThemeReportEmbedConfig";
+// API Endpoint to get the JSON response of Embed URL, Embed Token and reportId
+const reportEndpoint = "https://aka.ms/ThemeReportEmbedConfig";
 
-// Set the report refresh token timer
-const reportRefreshTokenTimer = 0;
+// Set minutes before the access token should get refreshed
+const minutesToRefreshBeforeExpiration = 2;
+
+// Store the amount of time left for refresh token
+let tokenExpiration;
 
 // This function will make the AJAX request to the endpoint and get the JSON response which it will set in the sessions
-function populateEmbedConfigIntoCurrentSession(url, updateCurrentToken) {
+function populateEmbedConfigIntoCurrentSession(updateCurrentToken) {
 
     try {
         let configFromParentWindow = window.parent.showcases.personalizeReportDesign;
@@ -22,7 +25,7 @@ function populateEmbedConfigIntoCurrentSession(url, updateCurrentToken) {
                 MinutesToExpiration: diffMins,
             };
 
-            handleNewEmbedConfig(url, embedConfig, updateCurrentToken);
+            handleNewEmbedConfig(embedConfig, updateCurrentToken);
         }
 
         return;
@@ -31,57 +34,63 @@ function populateEmbedConfigIntoCurrentSession(url, updateCurrentToken) {
     }
 
     // This returns the JSON response
-    return $.getJSON(url, function (embedConfig) {
-        handleNewEmbedConfig(url, embedConfig, updateCurrentToken);
+    return $.getJSON(reportEndpoint, function (embedConfig) {
+        handleNewEmbedConfig(embedConfig, updateCurrentToken);
     });
 }
 
-function tokenExpirationRefreshListener(minutesToExpiration, url, entityType) {
+function handleNewEmbedConfig(embedConfig, updateCurrentToken) {
 
-    // Used to count the milliseconds after which API call will be made
-    const updateAfterMilliSeconds = (minutesToExpiration - 2) * 60 * 1000;
-
-    // Set the tokenRefresh timer to count the seconds and request the JSON again when token expires
-    setTokenRefreshListener(updateAfterMilliSeconds, reportRefreshTokenTimer, url, entityType);
-}
-
-function handleNewEmbedConfig(tokenRefreshUrl, embedConfig, updateCurrentToken) {
-    // Set the embedToken, embedUrl, reportId
+    // Set Embed Token, Embed URL and Report Id
     setConfig(embedConfig.EmbedToken.Token, embedConfig.EmbedUrl, embedConfig.Id);
     if (updateCurrentToken) {
 
         // Get the reference to the embedded element
-        let reportContainer = $("#report-container")[0];
-        let embedContainer = powerbi.get(reportContainer);
-
-        if (embedContainer) {
-            embedContainer.setAccessToken(embedConfig.EmbedToken.Token);
+        const reportContainer = $(".report-container").get(0);
+        if (reportContainer) {
+            const report = powerbi.get(reportContainer);
+            report.setAccessToken(embedConfig.EmbedToken.Token);
         }
     }
-    tokenExpirationRefreshListener(embedConfig.MinutesToExpiration, tokenRefreshUrl, "Report");
+
+    // Get the milliseconds after token will get refreshed
+    tokenExpiration = embedConfig.MinutesToExpiration * 60 * 1000;
+
+    // Set the tokenRefresh timer to count the seconds and request the JSON again when token expires
+    setTokenExpirationListener();
 }
 
-// Checking the remaining time and calling the API again
-function setTokenRefreshListener(updateAfterMilliSeconds, refreshTokenTimer, url, entityType) {
-    if (refreshTokenTimer) {
-        console.log("step current " + entityType + " Embed Token update threads.");
-        clearTimeout(refreshTokenTimer);
+// Check the remaining time and call the API again
+function setTokenExpirationListener() {
+
+    const safetyInterval = minutesToRefreshBeforeExpiration * 60 * 1000;
+
+    // Time until token refresh in milliseconds
+    const timeout = tokenExpiration - safetyInterval;
+    if (timeout <= 0) {
+        populateEmbedConfigIntoCurrentSession(true /* updateCurrentToken */);
     }
-    console.log(entityType + " Embed Token will be updated in " + updateAfterMilliSeconds + " milliseconds.");
-
-    // Making the call when token expires
-    refreshTokenTimer = setTimeout(function () {
-        if (url) {
-            populateEmbedConfigIntoCurrentSession(url, true /* updateCurrentToken */); // It suggests that the token is expired so API request is made
-        }
-    }, updateAfterMilliSeconds);
+    else {
+        console.log(`Report Embed Token will be updated in ${timeout} milliseconds.`);
+        setTimeout(function () {
+            populateEmbedConfigIntoCurrentSession(true /* updateCurrentToken */)
+        }, timeout);
+    }
 }
 
-// Get the data from the API and pass it to the front
+// Add a listener to make sure token is updated after tab was inactive
+document.addEventListener("visibilitychange", function () {
+    // Check the access token when the tab is visible
+    if (!document.hidden) {
+        setTokenExpirationListener();
+    }
+});
+
+// Get the data from the API and pass it to the front-end
 function loadThemesShowcaseReportIntoSession() {
 
     // Call the function for the first time to call the API, set the sessions values and return the response to the front-end
-    return populateEmbedConfigIntoCurrentSession(reportUrl, false /* updateCurrentToken */);
+    return populateEmbedConfigIntoCurrentSession(false /* updateCurrentToken */);
 }
 
 // Set the embed config in globals
